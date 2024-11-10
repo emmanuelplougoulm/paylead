bc527648664cb81655138f97df3d39de7199c27c
viktor_vaughn <emmanuel.plougoulm@gmail.com>
2024-11-10 11:51:36 +0100

    test: add Playwright visibility test on load

--- app/e2e/vue.spec.js
+++ app/e2e/vue.spec.js
@@ -1,8 +1,26 @@
 import { test, expect } from '@playwright/test';
 
-// See here how to get started:
-// https://playwright.dev/docs/intro
-test('visits the app root url', async ({ page }) => {
-  await page.goto('/');
-  await expect(page.locator('div.greetings > h1')).toHaveText('You did it!');
-})
+test('Checks the presence of elements on the board', async ({ page }) => {
+    await page.goto('http://localhost:5173/');
+
+    const todoList = page.locator('.list-name:has-text("TODO")');
+    await expect(todoList).toBeVisible();
+
+    const pendingList = page.locator('.list-name:has-text("PENDING")');
+    await expect(pendingList).toBeVisible();
+
+    const doneList = page.locator('.list-name:has-text("DONE")');
+    await expect(doneList).toBeVisible();
+
+    const deleteButtons = page.locator('button:has-text("Delete List")');
+    await expect(deleteButtons).toHaveCount(3);
+
+    const addTicketButtons = page.locator('button:has-text("+ Add a ticket")');
+    await expect(addTicketButtons).toHaveCount(3);
+
+    const listNameInput = page.locator('.list-add input[placeholder="List name"]');
+    await expect(listNameInput).toBeVisible();
+
+    const addListButton = page.locator('.list-add button:has-text("Add a list")');
+    await expect(addListButton).toBeVisible();
+});
